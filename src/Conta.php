9400c9755f4329a10f7575f966681b6f4bf7117d
jmarciosilva <jmarciosilva@gmail.com>
2022-10-11 19:09:46 -0300

    Adicionado o método trasfere

--- src/Conta.php
+++ src/Conta.php
@@ -1,28 +1,35 @@
 <?php
 class Conta
 {
-   public string $cpfTitular;
-   public string $nomeTitular;
-   public float $saldo;
-   
-   public function saca(float $valorASacar):void
-   {
-        if($valorASacar > $this->saldo){
+    public string $cpfTitular;
+    public string $nomeTitular;
+    public float $saldo = 0;
+
+    public function saca(float $valorASacar): void
+    {
+        if ($valorASacar > $this->saldo) {
             echo "Operação não Realizada, SALDO INDISPONÍVEL";
             return;
         }
         $this->saldo -= $valorASacar;
-   }
+    }
 
-   public function deposita(float $valorADepositar):void
-   {
-        if($valorADepositar < 0){
+    public function deposita(float $valorADepositar): void
+    {
+        if ($valorADepositar < 0) {
             echo "Operação não Realizada, SOMENTE VALORES POSITIVOS";
             return;
         }
         $this->saldo += $valorADepositar;
-   }
+    }
 
-
-
+    public function transfere(float $valorATransferir, Conta $contaDestino): void
+    {
+        if ($valorATransferir > $this->saldo) {
+            echo "Operação não Realizada, SALDO INDISPONÍVEL";
+            return;
+        }
+        $this->saca($valorATransferir);
+        $contaDestino->deposita($valorATransferir);
+    }
 }
