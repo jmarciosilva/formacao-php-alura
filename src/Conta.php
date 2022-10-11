857c71c69a9e5ce3c6e52b00a270da2950bc0800
jmarciosilva <jmarciosilva@gmail.com>
2022-10-11 18:56:51 -0300

    Adicionado os métodos saca e deposita

--- src/Conta.php
+++ src/Conta.php
@@ -4,4 +4,25 @@ class Conta
    public string $cpfTitular;
    public string $nomeTitular;
    public float $saldo;
+   
+   public function saca(float $valorASacar):void
+   {
+        if($valorASacar > $this->saldo){
+            echo "Operação não Realizada, SALDO INDISPONÍVEL";
+            return;
+        }
+        $this->saldo -= $valorASacar;
+   }
+
+   public function deposita(float $valorADepositar):void
+   {
+        if($valorADepositar < 0){
+            echo "Operação não Realizada, SOMENTE VALORES POSITIVOS";
+            return;
+        }
+        $this->saldo += $valorADepositar;
+   }
+
+
+
 }
