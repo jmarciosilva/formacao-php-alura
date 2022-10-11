fc6c1979e373872ae1faef524a309d9492da0341
jmarciosilva <jmarciosilva@gmail.com>
2022-10-11 19:52:01 -0300

    Realizado o código para testar abstração e encapsulamento dos atributos e da classe Conta

--- banco.php
+++ banco.php
@@ -3,5 +3,9 @@ require_once 'src/Conta.php';
 
 $primeiraConta = new Conta();
 $primeiraConta->deposita(500);
+$primeiraConta->defineCpfTitular('123.456.789-10');
+$primeiraConta->defineNomeTitular("José Marcio");
+echo $primeiraConta->recuperaSaldo() . PHP_EOL;
+echo $primeiraConta->recuperaNomeTitular() . PHP_EOL;
+echo $primeiraConta->recuperaCpfTitular() . PHP_EOL;
 
-echo $primeiraConta->recuperaSaldo();
