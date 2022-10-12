c0c7a6c2ba8cf48faf9d69a757c90b5d2a12111b
jmarciosilva <jmarciosilva@gmail.com>
2022-10-12 19:12:53 -0300

    foi alterado o arquivo banco para realizar as apresentações dos dados com a nova classe Titular criada

--- banco.php
+++ banco.php
@@ -1,9 +1,10 @@
 <?php
 require_once 'src/Conta.php';
+require_once 'src/Titular.php';
 
-$primeiraConta = new Conta('123.456.789-10', 'José Márcio');
+$primeiraConta = new Conta(new Titular('123.456.789-10', 'José Márcio'));
 //var_dump($primeiraConta);
-$segundaConta = new Conta('987.654.321-10', 'Livia Ferreira');
+$segundaConta = new Conta(new Titular('987.654.321-10', 'Livia Ferreira'));
 
 $primeiraConta->deposita(500);
 echo "Nome do Titular: ". $primeiraConta->recuperaNomeTitular() . PHP_EOL;
