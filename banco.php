0b67dfc8f44c23c0b495fd96e7864c90b68fd6ce
jmarciosilva <jmarciosilva@gmail.com>
2022-10-12 17:47:19 -0300

    Foi feito testes com o método privado validaNomeTitular e verificado que o mesmo não pode ser acessado de outra classe

--- banco.php
+++ banco.php
@@ -5,7 +5,6 @@ $primeiraConta = new Conta('123.456.789-10', 'José Márcio');
 //var_dump($primeiraConta);
 $primeiraConta->deposita(500);
 
-
 echo $primeiraConta->recuperaNomeTitular() . PHP_EOL;
 echo $primeiraConta->recuperaCpfTitular() . PHP_EOL;
 echo $primeiraConta->recuperaSaldo() . PHP_EOL;
