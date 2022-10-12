dee22cc8d0e9f2bcae88ea50f5300e897828532e
jmarciosilva <jmarciosilva@gmail.com>
2022-10-12 17:18:25 -0300

    Inserido Construtor na classe Conta com parametros cpfTitular e nomeTitular removido da classe Conta métodos defineNomeTitular e defineCpfTitular

--- banco.php
+++ banco.php
@@ -1,11 +1,13 @@
 <?php
 require_once 'src/Conta.php';
 
-$primeiraConta = new Conta();
+$primeiraConta = new Conta('123.456.789-10', 'José Márcio');
+//var_dump($primeiraConta);
 $primeiraConta->deposita(500);
-$primeiraConta->defineCpfTitular('123.456.789-10');
-$primeiraConta->defineNomeTitular("José Marcio");
-echo $primeiraConta->recuperaSaldo() . PHP_EOL;
+
+
 echo $primeiraConta->recuperaNomeTitular() . PHP_EOL;
 echo $primeiraConta->recuperaCpfTitular() . PHP_EOL;
+echo $primeiraConta->recuperaSaldo() . PHP_EOL;
+
 
